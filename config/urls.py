61a187b82d497f76d0fac9b277cc3be45eeed449
nxxxtyetdecided <nxxxtyetdecided@gmail.com>
2022-05-09 12:36:48 +0900

    Fix: rest api에 맞게 url 수정

--- config/urls.py
+++ config/urls.py
@@ -40,8 +40,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('admin', admin.site.urls),
+    path('api-auth', include('rest_framework.urls')),
     path('restaurants', include('restaurants.urls')),
     path('results', include('results.urls')),
 
