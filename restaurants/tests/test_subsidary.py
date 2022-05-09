7695378de3c82e13eb0b960e5ed599791299de38
rsh1994 <rsh1994@naver.com>
2022-05-09 15:37:35 +0900

    Feat: Add Subsidary test code (soft-delete list)

--- restaurants/tests/test_subsidary.py
+++ restaurants/tests/test_subsidary.py
@@ -53,4 +53,25 @@ class Test_Subsidary:
         assert response.data['is_delete'] == True
         assert response.status_code == 200
 
-    
+    def test_subsidary_soft_delete_list(self, client, _setup):
+        # Soft-delete된 데이터가 list에 포함되지 않는지
+        temp = {
+            "is_delete": True
+        }
+
+        id = int(_setup.id)
+
+        # 데이터 하나를 Soft-delet한 상태로 넣어둔다.
+        soft_delete_data = client.put(
+            f'/restaurants/subsidary/{id}',
+            json.dumps(temp), content_type = "application/json"
+        )
+        
+        # 데이터 리스트를 조회한다.
+        response = client.get(
+            '/restaurants/subsidary'
+        )
+        assert len(response.data) == 0
+        assert response.status_code == 200
+        
+
